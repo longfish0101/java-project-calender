1db45ed821c0ba8d828d7bdbe740205820be2d2f
Jeon <Jeon@DESKTOP-4KT5B8G.lan>
2023-03-11 12:44:31 +0900

    add input.close

--- src/jeon_calender/sum.java
+++ src/jeon_calender/sum.java
@@ -2,14 +2,15 @@ package jeon_calender;
 import java.util.Scanner;
 public class sum {
 	public static void main(String[] args) {
-		System.out.println("두 수를 입력. ex) 5 10");
+		System.out.println("두 수를 입력. ex) 5,10");
 		Scanner input = new Scanner(System.in);
 		String num1 = input.nextLine();
-		String[] splitedNum = num1.split(" ");
+		String[] splitedNum = num1.split(",");
 		int num2 = Integer.parseInt(splitedNum[0]);
 		int num3 = Integer.parseInt(splitedNum[1]);
 		
 		int result = num2+num3;
 		System.out.println(result);
+		input.close();
 	}
 }
