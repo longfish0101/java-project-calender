1f69afd648dcebe574aee89a7deec7828f5c691d
Jeon <Jeon@DESKTOP-4KT5B8G.lan>
2023-03-11 21:51:09 +0900

    Update

--- src/jeon_calender/Month.java
+++ src/jeon_calender/Month.java
@@ -7,33 +7,32 @@ public class Month {
 	public static int maxDay(int a) {
 		return month[a-1];
 	}
+	
+	public static int repeat() {
+		System.out.println("반복할 횟수 입력");
+		Scanner scanner = new Scanner(System.in);
+		int repeat = scanner.nextInt();
+		return repeat;
+	}
+	
+	public static void print(int repeat) {
+		for(int i=0; i<repeat; i++) {
+			int count=0;
+			System.out.println("월을 입력(1~12)");
+			while(count == 0) {
+			Scanner scanner = new Scanner(System.in);
+			int input = scanner.nextInt();
+				if (input >= 1 && input <= 12) {
+					System.out.printf("%d월은 총 %d일로 구성되어 있습니다\n", input, maxDay(input));
+					count++;
+				} else {
+				System.out.println("1~12만 입력");
+				}
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
-		
-		Scanner scanner1 = new Scanner(System.in);
-		int loop = scanner1.nextInt();
-		int[] loopForMonth = new int[loop];
-		int count = 0;
-		while(count < loop) {
-			Scanner scanner2 = new Scanner(System.in);
-			int loop2 = scanner1.nextInt();
-			loopForMonth[count] = loop2;
-			count++;
-		}
-		
-		for (int i=0; i<loopForMonth.length; i++) {
-			System.out.printf("%d월은 총 %d일로 구성되어 있습니다\n", loopForMonth[i], maxDay(loopForMonth[i]));
-
-		}
-//		for (int i = 0; i < 1;) {
-//			Scanner scanner = new Scanner(System.in);
-//			int num = scanner.nextInt();
-//			if (num >= 1 && num <= 12) {
-//				System.out.printf("%d월은 총 %d일로 구성되어 있습니다", num, maxDay(num));
-//				i++;
-//				scanner.close();
-//			} else {
-//				System.out.println("1~12사이를 입력해 주세요");
-//			}
-//		}
+		print(repeat());
 	}
 }
